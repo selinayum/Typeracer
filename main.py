0de7d63a7ecc2371fb0d1210fa120c48c82b4072
Catherine Laserna <49669196+cjlaserna@users.noreply.github.com>
2023-05-29 08:07:36 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+import sys
+from pygame.locals import *
+
+# width and height
+# Text color
+# bg color
+
+
+class Constants:
+    WIDTH = 750
+    HEIGHT = 500
+    TEXT_COLOR = (255, 232, 123)
+    BG_COLOR = (66, 66, 66)
+
+
+WINDOW = pygame.display.set_mode((Constants.WIDTH, Constants.HEIGHT))
+pygame.display.set_caption('Typeracer >:)')
+
+pygame.init()
+
+while True:
+    pygame.display.update()
